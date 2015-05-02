17160a31bb408e324ea7497875f896584f4c99cf
anil1989 <anilugale1989@gmail.com>
2015-05-02 16:23:00 +0530

    dsgfd

--- app/src/main/java/com/rmc/rmc/Master.java
+++ app/src/main/java/com/rmc/rmc/Master.java
@@ -2,7 +2,6 @@ package com.rmc.rmc;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.webkit.WebView;
 
 
 public class Master extends ActionBarActivity {
@@ -11,14 +10,7 @@ public class Master extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master);
-        WebView wv;
-        wv = (WebView) findViewById(R.id.index);
 
-        wv.clearCache(true);
-        wv.clearHistory();
-        wv.getSettings().setJavaScriptEnabled(true);
-        wv.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        wv.loadUrl("file:///android_asset/index.html");
     }
 
 
